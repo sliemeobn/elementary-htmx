84e6ae465ca165ab3a4d307518012b13475f086c
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2024-06-26 10:21:22 +0200

    added vapor example

---
+++ Examples/VaporDemo/Sources/App/Routes.swift
@@ -0,0 +1,18 @@
+import Elementary
+import Vapor
+
+func addRoutes(to app: Application) {
+    app.get("") { _ in
+        HTMLResponse {
+            MainPage()
+        }
+    }
+
+    app.get("result") { request in
+        let x = try request.query.get(Int.self, at: "x")
+        let y = try request.query.get(Int.self, at: "y")
+        return HTMLResponse {
+            ResultView(x: x, y: y)
+        }
+    }
+}
