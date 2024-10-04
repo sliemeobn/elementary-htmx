0d4da815f16fd56729a14492e5c71dd15c8fb5d9
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2024-10-04 17:05:18 +0200

    update example dependencies (#12)

--- Examples/VaporDemo/Package.swift
+++ Examples/VaporDemo/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", from: "4.102.0"),
-        .package(url: "https://github.com/vapor-community/vapor-elementary.git", from: "0.1.0"),
+        .package(url: "https://github.com/vapor-community/vapor-elementary.git", from: "0.2.0"),
         .package(path: "../../"),
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
     ],
